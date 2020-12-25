bcc8430ef1f55f6f6c29d9e14cf7a14556f794c7
Ilya Kuznetsov <ikuz2002@gmail.com>
2020-12-25 17:22:37 +0300

    chore: Prepare for deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -Dspring.profiles.active=heroku $JAVA_OPTS -jar "$SERVICE/build/libs/$SERVICE-0.0.1-SNAPSHOT.jar"
---
+++ authenticator/src/main/java/com/taskmanager/authenticator/config/EurekaConfig.java
@@ -0,0 +1,33 @@
+package com.taskmanager.authenticator.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@ConfigurationProperties(prefix = "eureka.instance")
+public class EurekaConfig {
+
+    private String hostname;
+    private boolean nonSecurePortEnabled;
+    private int nonSecurePort;
+    private boolean securePortEnabled;
+    private int securePort;
+
+    @Bean
+    @Profile("heroku")
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+        b.setHostname(hostname);
+        b.setNonSecurePortEnabled(nonSecurePortEnabled);
+        if (nonSecurePortEnabled)
+            b.setNonSecurePort(nonSecurePort);
+        b.setSecurePortEnabled(securePortEnabled);
+        if (securePortEnabled)
+            b.setSecurePort(securePort);
+        return b;
+    }
+}
